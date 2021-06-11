e73d5f480d9da5884a4fe9ac0d0eb60994801bb5
shaw-555 <shaw18664590570@163.com>
2021-06-11 13:37:51 +0800

    作用域插槽

--- src/router.js
+++ src/router.js
@@ -8,42 +8,6 @@ const router = new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      // name: 'index',
-      component: () => import("./views/index.vue"),
-      children: [
-        {
-          path: "",
-          redirect: "/home"
-        },
-        {
-          path: "/home",
-          name: "home",
-          component: () => import("./views/Home.vue")
-        },
-        {
-          path: "/order",
-          name: "order",
-          component: () => import("./views/Order.vue")
-        },
-        {
-          path: "/me",
-          name: "me",
-          component: () => import("./views/Me.vue")
-        },
-        {
-          path: "/address",
-          name: "address",
-          component: () => import("./views/Address.vue")
-        },
-        {
-          path: "/city",
-          name: "city",
-          component: () => import("./views/City.vue")
-        }
-      ]
-    },
-    {
       path: "/login",
       name: "login",
       component: () => import("./views/Login.vue")
---
+++ src/views/TheSlot.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    3中插槽
+    <slot>这是默认值</slot>
+    <slot name="hello"></slot>
+    <slot name="scope1" :msg="msg"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheSlot',
+  data () {
+    return {
+      msg: {
+        a:'the test for props',
+        b:'hhh'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  div {
+    background-color: aqua;
+  }
+</style>
